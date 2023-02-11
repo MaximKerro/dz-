753f8749caaec4512c8999874f3b956b92c615ae
Maxim <sunforge@yandex.ru>
2023-02-11 16:01:22 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Даны 2 числа, показать наибольшее  и наименьшее из них
+int numberA = 15;
+int numberB = 30;
+
+int min = 15;
+int max = 30;
+
+if (numberA < numberB) min = numberA;
+Console.Write("Минимальное число: ");
+Console.WriteLine(min);
+
+if (numberB > numberA) max= numberB;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
+
+
