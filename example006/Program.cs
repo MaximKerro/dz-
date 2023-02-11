5012a9890ff2a37c0892a84e553fc3f95d25a106
Maxim <sunforge@yandex.ru>
2023-02-11 16:02:21 +0300

    excasd

--- example006/Program.cs
+++ example006/Program.cs
@@ -1,9 +1,8 @@
-﻿int xa = 40;
-int ya = 1;
-int xb = 1; 
-int yb = 30; 
-int xc = 80;
-int yc = 30;
+﻿Console.Clear();
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30, 
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -14,24 +13,24 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc,yc);
 Console.WriteLine("+");
 
-int x = xa, y = xb; 
+int x = xa, y = xa; 
  
 
 int count = 0;
 
-while(count < 10000)
+while (count < 100)
 {
     int what = new Random().Next(0, 3);
     if(what == 0)
     {
-        x = (x+xa)/2;
-        y = (y+ya)/2;
+        x = (x + xa)/2;
+        y = (y + ya)/2;
     }
 
     if(what == 2)
     {
-        x = (x+xc)/2;
-        y = (y+yc)/2;
+        x = (x + xc)/2;
+        y = (y + yc)/2;
     }
 
     Console.SetCursorPosition(x, y);
