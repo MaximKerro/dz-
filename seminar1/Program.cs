6433d61296d1719c9015b244ae65b619a1ab2818
Maxim <sunforge@yandex.ru>
2023-02-11 16:01:42 +0300

    seminar1

---
+++ seminar1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write ("Введите ваше число: ");
+int value = int.Parse(Console.ReadLine()!);
+int numberA = value;
+Console.Write ("Ваш ответ равен: ");
+Console.WriteLine (numberA * numberA); 
+
+
+
